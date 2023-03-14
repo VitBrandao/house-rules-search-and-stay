decef0c63a8367c13cdf17b7e8f11fa986ce12cd
VitBrandao <vitorsbsilva@gmail.com>
2023-03-14 12:36:35 -0300

    feat: Header

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p><a href="/"> HOUSE RULES </a></p>
+        <p><a href="/create"> CREATE NEW RULE </a></p>
+        <p><a href="/requests"> TESTING REQUESTS </a></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
