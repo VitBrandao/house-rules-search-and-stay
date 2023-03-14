801ce57c894b18459e190a74f395d25e10deb161
VitBrandao <vitorsbsilva@gmail.com>
2023-03-14 14:09:49 -0300

    style: Header

--- components/Header.vue
+++ components/Header.vue
@@ -11,3 +11,27 @@ export default {
     name: 'Header',
 }
 </script>
+
+<style scoped>
+    div {
+        list-style: none;
+        display: flex;
+        background: black;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+
+    p {
+        margin-right: 10px;
+    }
+
+    a {
+        color: #DDD;
+        text-decoration: none;
+        transition: .5s;
+    }
+
+    a:hover {
+        color: turquoise;
+    }
+</style>
